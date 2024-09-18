d25215fbcc131fd068cbf9349500985ac6bdeb60
Gerardo <angel.orozco.gzl@gmail.com>
2024-09-18 17:34:15 -0500

    añadiendo y corriguiendo archivos del profe

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pusher
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("pago_curso.html")
-
+    return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
@@ -19,3 +20,15 @@ def alumnosGuardar():
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
+
+@app.route("/evento")
+def evento():
+    pusher_client = pusher.Pusher(
+        app_id="1714541",
+        key="cda1cc599395d699a2af",
+        secret="9e9c00fc36600060d9e2",
+        cluster="us2",
+        ssl=True
+    )
+    
+    pusher_client.trigger("my-channel", "my-event", {})
