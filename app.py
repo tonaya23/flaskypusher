334fe9a891be01d95d47497f0ebf5aa6d737dcf1
Gerardo <angel.orozco.gzl@gmail.com>
2024-09-18 18:27:23 -0500

    codigo actualizado

--- app.py
+++ app.py
@@ -5,24 +5,49 @@ from flask import request
 
 import pusher
 
+import mysql.connector
+import datetime
+import pytz
+
+con = mysql.connector.connect(
+  host="185.232.14.52",
+  database="u760464709_tst_sep",
+  user="u760464709_tst_sep_usr",
+  password="dJ0CIAFF="
+)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    con.close()
     return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
+    con.close()
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
+    con.close()
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
+@app.route("/buscar")
+def buscar()
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM sensor_log")
+    
+    registros = mycursor.fetchall()
+
+    return registros
+
 @app.route("/registrar", methods=["GET"])
-def evento():
+def registrar():
     pusher_client = pusher.Pusher(
         app_id = "1867154"
         key = "84b47e81b86f0dd58c26"
@@ -30,5 +55,5 @@ def evento():
         cluster = "us2"
         ssl=True
     )
-    
-    pusher_client.trigger("canalRegistrosTemperatura", "registroTemperatura", {})
+
+    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", request.args)
