b65b9f8fcb6100f2e0bafcff9388a40246fa370e
Gerardo <angel.orozco.gzl@gmail.com>
2024-09-18 17:45:11 -0500

    nombres intuitivos

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def alumnosGuardar():
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
-@app.route("/evento")
+@app.route("/registrar", methods=["GET"])
 def evento():
     pusher_client = pusher.Pusher(
         app_id = "1867154"
@@ -31,4 +31,4 @@ def evento():
         ssl=True
     )
     
-    pusher_client.trigger("my-channel", "my-event", {})
+    pusher_client.trigger("canalRegistrosTemperatura", "registroTemperatura", {})
