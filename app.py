30363a476f68a2fe85f310fb6a9232444c0ba8e9
Gerardo <angel.orozco.gzl@gmail.com>
2024-09-23 04:00:00 -0500

    devolviendo args

--- app.py
+++ app.py
@@ -33,37 +33,31 @@ def buscar():
 
 @app.route("/registrar", methods=["GET"])
 def registrar():
-    telefono = request.args.get('telefono')
-    archivo = request.args.get('archivo')
+    args = request.args
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+    sql = "INSERT INTO curso_pago (Telefono, Archivo) VALUES (%s, %s)"
+    val = (args["telefono"], args["archivo"])
+    cursor.execute(sql, val)
     
-    if telefono and archivo:
-        # Insertar en la base de datos
-        if not con.is_connected():
-            con.reconnect()
-        cursor = con.cursor()
-        query = "INSERT INTO curso_pago (Telefono, Archivo) VALUES (%s, %s)"
-        cursor.execute(query, (telefono, archivo))
-        con.commit()
-        
-        # Obtener el ID del curso recién insertado
-        id_curso_pago = cursor.lastrowid
-        
-        # Configurar Pusher
-        pusher_client = pusher.Pusher(
-            app_id="1867154",
-            key="84b47e81b86f0dd58c26",
-            secret="a1408f8b96c7eba73b16",
-            cluster="us2",
-            ssl=True
-        )
-
-        # Enviar los datos a través de Pusher
-        pusher_client.trigger("canalRegistrosCursoPago", "registroCursoPago", {
-            "Id_Curso_Pago": id_curso_pago,
-            "Telefono": telefono,
-            "Archivo": archivo
-        })
-
-        return f"Registro exitoso. ID: {id_curso_pago}, Teléfono: {telefono}, Archivo: {archivo}"
-    else:
-        return "Error: Se requieren los parámetros 'telefono' y 'archivo'", 400
+    con.commit()
+    id_curso_pago = cursor.lastrowid
+    con.close()
+    
+    pusher_client = pusher.Pusher(
+        app_id="1867154",
+        key="84b47e81b86f0dd58c26",
+        secret="a1408f8b96c7eba73b16",
+        cluster="us2",
+        ssl=True
+    )
+    
+    pusher_data = {
+        "Id_Curso_Pago": id_curso_pago,
+        "Telefono": args["telefono"],
+        "Archivo": args["archivo"]
+    }
+    
+    pusher_client.trigger("canalRegistrosCursoPago", "registroCursoPago", pusher_data)
+    return pusher_data
